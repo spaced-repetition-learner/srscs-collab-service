a0d132b3544e19be0e9fbd19897f0c0be7ec7caf
dadepu <danielkoellgen@gmail.com>
2022-03-10 06:47:17 +0100

    initial

---
+++ src/main/java/de/danielkoellgen/srscscollabservice/SrscsCollabServiceApplication.java
@@ -0,0 +1,13 @@
+package de.danielkoellgen.srscscollabservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrscsCollabServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrscsCollabServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/danielkoellgen/srscscollabservice/SrscsCollabServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.danielkoellgen.srscscollabservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrscsCollabServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
