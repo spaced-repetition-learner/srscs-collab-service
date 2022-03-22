58d81aa9800b39b23e099ae9ab9462071652e220
dadepu <danielkoellgen@gmail.com>
2022-03-22 08:50:36 +0100

    refactor(deck): make deck-entity null safe

--- src/main/java/de/danielkoellgen/srscscollabservice/domain/deck/domain/Deck.java
+++ src/main/java/de/danielkoellgen/srscscollabservice/domain/deck/domain/Deck.java
@@ -1,5 +1,6 @@
 package de.danielkoellgen.srscscollabservice.domain.deck.domain;
 
+import de.danielkoellgen.srscscollabservice.domain.core.IllegalEntityPersistenceState;
 import de.danielkoellgen.srscscollabservice.domain.user.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,7 +16,13 @@ public class Deck {
     @NotNull
     private final UUID deckId;
 
-    @Getter
     @Nullable
     private User user;
+
+    public @NotNull User getUser() {
+        if (user == null) {
+            throw new IllegalEntityPersistenceState("[user] not instantiated while trying to access it.");
+        }
+        return user;
+    }
 }
