88f0ff8bbda1a60e2d5b621b0273605023e24553
dadepu <danielkoellgen@gmail.com>
2022-03-22 06:18:37 +0100

    test(user): make repository-test evaluate both user-repositories

--- src/test/java/de/danielkoellgen/srscscollabservice/integrationTests/domain/user/repository/UserRepositoryIntegrationTest.java
+++ src/test/java/de/danielkoellgen/srscscollabservice/integrationTests/domain/user/repository/UserRepositoryIntegrationTest.java
@@ -3,6 +3,7 @@ package de.danielkoellgen.srscscollabservice.integrationTests.domain.user.reposi
 import de.danielkoellgen.srscscollabservice.domain.domainprimitives.Username;
 import de.danielkoellgen.srscscollabservice.domain.user.domain.User;
 import de.danielkoellgen.srscscollabservice.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,22 +11,38 @@ import org.springframework.data.cassandra.core.CassandraOperations;
 
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 public class UserRepositoryIntegrationTest {
 
-    private final CassandraOperations cassandraTemplate;
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserRepositoryIntegrationTest(CassandraOperations cassandraTemplate) {
-        this.cassandraTemplate = cassandraTemplate;
+    public UserRepositoryIntegrationTest(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @AfterEach
+    public void cleanUp() {
     }
 
     @Test
-    public void shouldSaveAndLoadUser() throws Exception {
+    public void shouldAllowToSaveAndLoadUser() throws Exception {
         // given
         User user = new User(UUID.randomUUID(), new Username("dadepu"), true);
 
         // when
-        cassandraTemplate.insert(user);
+        userRepository.save(user);
+
+        // then
+        User userById = userRepository.findUserByUserId(user.getUserId()).get();
+        assertThat(userById.getUsername())
+                .isEqualTo(userById.getUsername());
+
+        // and then
+        User userByUsername = userRepository.findUserByUsername(user.getUsername()).get();
+        assertThat(userByUsername.getUsername())
+                .isEqualTo(userByUsername.getUsername());
     }
 }
