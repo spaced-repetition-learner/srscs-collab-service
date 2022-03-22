05740a35c477f663432785c00cf4729f6ff6463b
dadepu <danielkoellgen@gmail.com>
2022-03-22 13:39:52 +0100

    feat(collaboration): add mapping to status

--- src/main/java/de/danielkoellgen/srscscollabservice/domain/collaboration/domain/ParticipantStatus.java
+++ src/main/java/de/danielkoellgen/srscscollabservice/domain/collaboration/domain/ParticipantStatus.java
@@ -1,8 +1,30 @@
 package de.danielkoellgen.srscscollabservice.domain.collaboration.domain;
 
+import de.danielkoellgen.srscscollabservice.domain.core.IllegalMappingException;
+import org.jetbrains.annotations.NotNull;
+
 public enum ParticipantStatus {
     INVITED,
     INVITATION_ACCEPTED,
     INVITATION_DECLINED,
-    TERMINATED
+    TERMINATED;
+
+    public static @NotNull Integer fromEnum(@NotNull ParticipantStatus status) {
+        return switch (status) {
+            case INVITED -> 0;
+            case INVITATION_ACCEPTED -> 1;
+            case INVITATION_DECLINED -> 2;
+            case TERMINATED -> 3;
+        };
+    }
+
+    public static @NotNull ParticipantStatus fromNumber(@NotNull Integer number) {
+        return switch (number) {
+            case 0 -> INVITED;
+            case 1 -> INVITATION_ACCEPTED;
+            case 2 -> INVITATION_DECLINED;
+            case 3 -> TERMINATED;
+            default -> throw new IllegalMappingException("Failed to map ParticipantStatus. Number is [" + number + "].");
+        };
+    }
 }
