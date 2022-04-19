773161ad2baeef2474dfff05c0647a46035c1a84
dadepu <danielkoellgen@gmail.com>
2022-04-19 07:26:20 +0200

    feat(user): allow to disable Users

--- src/main/java/de/danielkoellgen/srscscollabservice/domain/user/application/UserService.java
+++ src/main/java/de/danielkoellgen/srscscollabservice/domain/user/application/UserService.java
@@ -4,9 +4,12 @@ import de.danielkoellgen.srscscollabservice.domain.domainprimitives.Username;
 import de.danielkoellgen.srscscollabservice.domain.user.domain.User;
 import de.danielkoellgen.srscscollabservice.domain.user.repository.UserRepository;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 @Service
@@ -14,22 +17,30 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private final Logger logger = LoggerFactory.getLogger(UserService.class);
+
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public void addExternallyCreatedUser(@NotNull UUID transactionId, @NotNull UUID userId, @NotNull Username username,
-            @NotNull Boolean isActive) {
-        User user = new User(userId, username, isActive);
+    public @NotNull User addExternallyCreatedUser(@NotNull UUID transactionId, @NotNull UUID userId, @NotNull Username username) {
+        User user = User.makeNewUser(userId, username);
         userRepository.save(user);
+
+        logger.info("New User '{}' added. [tid={}, userId={}]",
+                user.getUsername().getUsername(), transactionId, user.getUserId());
+        logger.trace("New User added. [{}]", user);
+
+        return user;
     }
 
-    public void changeExternallyChangedUsername(UUID transactionId) {
-        //TODO
-    }
+    public void disableExternallyDisabledUser(UUID transactionId, UUID userId) throws NoSuchElementException {
+        User user = userRepository.findUserByUserId(userId).get();
+        user.disableUser();
+        userRepository.save(user);
 
-    public void updateExternallyUpdatedUserToDisabled(UUID transactionId) {
-        //TODO
+        logger.info("User '{}' disabled. [tid={}, userId={}]",
+                user.getUsername().getUsername(), transactionId, user.getUserId());
     }
 }
