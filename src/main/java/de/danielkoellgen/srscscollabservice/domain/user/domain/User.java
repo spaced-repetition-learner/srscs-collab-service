b76dab69ed2fe6b774208bf110ac12b45f9f50b1
dadepu <danielkoellgen@gmail.com>
2022-04-19 07:25:02 +0200

    refactor(user): reorganize User-constructors

--- src/main/java/de/danielkoellgen/srscscollabservice/domain/user/domain/User.java
+++ src/main/java/de/danielkoellgen/srscscollabservice/domain/user/domain/User.java
@@ -1,45 +1,36 @@
 package de.danielkoellgen.srscscollabservice.domain.user.domain;
 
-import de.danielkoellgen.srscscollabservice.domain.core.IllegalEntityPersistenceState;
 import de.danielkoellgen.srscscollabservice.domain.domainprimitives.Username;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.UUID;
 
-@AllArgsConstructor
+@Getter
 public class User {
 
-    @Getter
     @NotNull
     private final UUID userId;
 
-    @Nullable
-    private Username username;
+    @NotNull
+    private final Username username;
 
-    @Nullable
+    @NotNull
     private Boolean isActive;
 
+    public User(@NotNull UUID userId, @NotNull Username username, @NotNull Boolean isActive) {
+        this.userId = userId;
+        this.username = username;
+        this.isActive = isActive;
+    }
 
-    public Boolean disableUser() {
+    public static @NotNull User makeNewUser(@NotNull UUID userId, @NotNull Username username) {
+        return new User(userId, username, true);
+    }
+
+    public void disableUser() {
         isActive = false;
-        return true;
-    }
-
-    public @NotNull Username getUsername() {
-        if (username == null) {
-            throw new IllegalEntityPersistenceState("[username] not instantiated while trying to access it.");
-        }
-        return username;
-    }
-
-    public @NotNull Boolean getIsActive() {
-        if (isActive == null) {
-            throw new IllegalEntityPersistenceState("[isActive] not instantiated while trying to access it.");
-        }
-        return isActive;
     }
 
     @Override
