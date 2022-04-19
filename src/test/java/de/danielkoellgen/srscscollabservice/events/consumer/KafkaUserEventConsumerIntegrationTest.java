d08a5d3dcc85b556fd61c17ad278c966a3696ce3
dadepu <danielkoellgen@gmail.com>
2022-04-19 08:26:05 +0200

    test(events): should disable User when receiving UserDisabled event

--- src/test/java/de/danielkoellgen/srscscollabservice/events/consumer/KafkaUserEventConsumerIntegrationTest.java
+++ src/test/java/de/danielkoellgen/srscscollabservice/events/consumer/KafkaUserEventConsumerIntegrationTest.java
@@ -2,11 +2,14 @@ package de.danielkoellgen.srscscollabservice.events.consumer;
 
 import de.danielkoellgen.srscscollabservice.core.converter.ProducerEventToConsumerRecordConverter;
 import de.danielkoellgen.srscscollabservice.core.events.producer.UserCreatedProd;
+import de.danielkoellgen.srscscollabservice.core.events.producer.UserDisabledProd;
 import de.danielkoellgen.srscscollabservice.domain.domainprimitives.Username;
+import de.danielkoellgen.srscscollabservice.domain.user.application.UserService;
 import de.danielkoellgen.srscscollabservice.domain.user.domain.User;
 import de.danielkoellgen.srscscollabservice.domain.user.repository.UserRepository;
 import de.danielkoellgen.srscscollabservice.events.consumer.user.KafkaUserEventConsumer;
 import de.danielkoellgen.srscscollabservice.events.consumer.user.dto.UserCreatedDto;
+import de.danielkoellgen.srscscollabservice.events.consumer.user.dto.UserDisabledDto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,17 +25,20 @@ public class KafkaUserEventConsumerIntegrationTest {
 
     private final KafkaUserEventConsumer kafkaUserEventConsumer;
 
-    private final ProducerEventToConsumerRecordConverter mapToConsumerRecordConverter;
+    private final UserService userService;
 
     private final UserRepository userRepository;
+
+    private final ProducerEventToConsumerRecordConverter mapToConsumerRecordConverter;
 
     private UUID userId;
     private Username username;
 
     @Autowired
-    public KafkaUserEventConsumerIntegrationTest(KafkaUserEventConsumer kafkaUserEventConsumer,
+    public KafkaUserEventConsumerIntegrationTest(KafkaUserEventConsumer kafkaUserEventConsumer, UserService userService,
             ProducerEventToConsumerRecordConverter mapToConsumerRecordConverter, UserRepository userRepository) {
         this.kafkaUserEventConsumer = kafkaUserEventConsumer;
+        this.userService = userService;
         this.mapToConsumerRecordConverter = mapToConsumerRecordConverter;
         this.userRepository = userRepository;
     }
@@ -69,5 +75,31 @@ public class KafkaUserEventConsumerIntegrationTest {
         User fetchedUserByUsername = userRepository.findUserByUsername(username).orElseThrow();
         assertThat(fetchedUserByUsername.getUserId())
                 .isEqualTo(userId);
+    }
+
+    @Test
+    public void shouldDisableUserWhenReceivingUserDisabledEvent() throws Exception {
+        // given
+        userService.addExternallyCreatedUser(
+                UUID.randomUUID(), userId, username
+        );
+        UserDisabledProd userDisabledProd = new UserDisabledProd(
+                UUID.randomUUID(), new UserDisabledDto(userId)
+        );
+
+        // when
+        kafkaUserEventConsumer.receive(
+                mapToConsumerRecordConverter.convert(userDisabledProd)
+        );
+
+        // then
+        User fetchedUserById = userRepository.findUserByUserId(userId).orElseThrow();
+        assertThat(fetchedUserById.getIsActive())
+                .isFalse();
+
+        // and then
+        User fetchedUserByUsername = userRepository.findUserByUsername(username).orElseThrow();
+        assertThat(fetchedUserByUsername.getIsActive())
+                .isFalse();
     }
 }
