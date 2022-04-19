294da57f3be0c4b769eef54d0cdaa420d073f59e
dadepu <danielkoellgen@gmail.com>
2022-04-19 07:00:40 +0200

    config: add Kafka-Config

---
+++ src/main/java/de/danielkoellgen/srscscollabservice/KafkaTopicConfig.java
@@ -0,0 +1,32 @@
+package de.danielkoellgen.srscscollabservice;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaTopicConfig {
+
+    private final String bootstrapAddress;
+
+    @Autowired
+    public KafkaTopicConfig(@Value("${kafka.bootstrapAddress}") String bootstrapAddress) {
+        this.bootstrapAddress = bootstrapAddress;
+    }
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
+}
