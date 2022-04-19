81b8d50add4e551a01fc35f71aca30abb2046103
dadepu <danielkoellgen@gmail.com>
2022-04-19 08:22:07 +0200

    test(events): should create User when receiving UserCreated Event

---
+++ src/test/java/de/danielkoellgen/srscscollabservice/events/consumer/KafkaUserEventConsumerIntegrationTest.java
@@ -0,0 +1,73 @@
+package de.danielkoellgen.srscscollabservice.events.consumer;
+
+import de.danielkoellgen.srscscollabservice.core.converter.ProducerEventToConsumerRecordConverter;
+import de.danielkoellgen.srscscollabservice.core.events.producer.UserCreatedProd;
+import de.danielkoellgen.srscscollabservice.domain.domainprimitives.Username;
+import de.danielkoellgen.srscscollabservice.domain.user.domain.User;
+import de.danielkoellgen.srscscollabservice.domain.user.repository.UserRepository;
+import de.danielkoellgen.srscscollabservice.events.consumer.user.KafkaUserEventConsumer;
+import de.danielkoellgen.srscscollabservice.events.consumer.user.dto.UserCreatedDto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class KafkaUserEventConsumerIntegrationTest {
+
+    private final KafkaUserEventConsumer kafkaUserEventConsumer;
+
+    private final ProducerEventToConsumerRecordConverter mapToConsumerRecordConverter;
+
+    private final UserRepository userRepository;
+
+    private UUID userId;
+    private Username username;
+
+    @Autowired
+    public KafkaUserEventConsumerIntegrationTest(KafkaUserEventConsumer kafkaUserEventConsumer,
+            ProducerEventToConsumerRecordConverter mapToConsumerRecordConverter, UserRepository userRepository) {
+        this.kafkaUserEventConsumer = kafkaUserEventConsumer;
+        this.mapToConsumerRecordConverter = mapToConsumerRecordConverter;
+        this.userRepository = userRepository;
+    }
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        userId = UUID.randomUUID();
+        username = new Username("dadepu");
+    }
+
+    @AfterEach
+    public void cleanUp() {
+
+    }
+
+    @Test
+    public void shouldCreateUserWhenReceivingUserCreatedEvent() throws Exception {
+        // given
+        UserCreatedProd userCreatedProd = new UserCreatedProd(
+                UUID.randomUUID(), new UserCreatedDto(userId, username.getUsername())
+        );
+
+        // when
+        kafkaUserEventConsumer.receive(
+                mapToConsumerRecordConverter.convert(userCreatedProd)
+        );
+
+        // then
+        User fetchedUser = userRepository.findUserByUserId(userId).orElseThrow();
+        assertThat(fetchedUser.getUsername())
+                .isEqualTo(username);
+
+        // and then
+        User fetchedUserByUsername = userRepository.findUserByUsername(username).orElseThrow();
+        assertThat(fetchedUserByUsername.getUserId())
+                .isEqualTo(userId);
+    }
+}
