1541d1a8bc8d24a94535635a54e9d0433a731e44
dadepu <danielkoellgen@gmail.com>
2022-04-19 07:13:06 +0200

    fix(user): remove duplicated domain primitive

--- src/main/java/de/danielkoellgen/srscscollabservice/domain/user/domain/UsernameException.java
+++
@@ -1,7 +0,0 @@
-package de.danielkoellgen.srscscollabservice.domain.user.domain;
-
-public class UsernameException extends Exception {
-    public UsernameException(String message) {
-        super(message);
-    }
-}
