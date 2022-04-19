630f81578d77597858e4460e3a25f49691216d29
dadepu <danielkoellgen@gmail.com>
2022-04-19 15:15:55 +0200

    feat(collaboration): add logging

--- src/main/java/de/danielkoellgen/srscscollabservice/domain/collaboration/application/CollaborationService.java
+++ src/main/java/de/danielkoellgen/srscscollabservice/domain/collaboration/application/CollaborationService.java
@@ -1,5 +1,6 @@
 package de.danielkoellgen.srscscollabservice.domain.collaboration.application;
 
+import de.danielkoellgen.srscscollabservice.controller.collaboration.CollaborationController;
 import de.danielkoellgen.srscscollabservice.domain.collaboration.domain.Collaboration;
 import de.danielkoellgen.srscscollabservice.domain.collaboration.domain.CollaborationStateException;
 import de.danielkoellgen.srscscollabservice.domain.collaboration.domain.Participant;
@@ -9,6 +10,8 @@ import de.danielkoellgen.srscscollabservice.domain.domainprimitives.Username;
 import de.danielkoellgen.srscscollabservice.domain.user.domain.User;
 import de.danielkoellgen.srscscollabservice.domain.user.repository.UserRepository;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,21 +25,31 @@ public class CollaborationService {
     private final CollaborationRepository collaborationRepository;
     private final UserRepository userRepository;
 
+    private final Logger logger = LoggerFactory.getLogger(CollaborationService.class);
+
     @Autowired
     public CollaborationService(CollaborationRepository collaborationRepository, UserRepository userRepository) {
         this.collaborationRepository = collaborationRepository;
         this.userRepository = userRepository;
     }
 
-    public UUID startNewCollaboration(@NotNull UUID transactionId, @NotNull DeckName name,
+    public Collaboration startNewCollaboration(@NotNull UUID transactionId, @NotNull DeckName collaborationName,
             @NotNull List<Username> usernames) throws NoSuchElementException {
-        List<User> users = usernames.stream().map(username ->
-                userRepository.findUserByUsername(username).get()
-        ).toList();
-
-        Collaboration collaboration = Collaboration.startNewCollaboration(transactionId, name, users);
+        List<User> invitedUsers = usernames.stream()
+                .map(username -> userRepository.findUserByUsername(username).get())
+                .toList();
+        Collaboration collaboration = Collaboration.startNewCollaboration(transactionId, collaborationName, invitedUsers);
         collaborationRepository.saveNewCollaboration(collaboration);
-        return collaboration.getCollaborationId();
+        logger.info("New Collaboration '{}' started with {} invited User(s). [tid={}]",
+                collaborationName.getName(),
+                invitedUsers.size(),
+                transactionId
+        );
+        logger.trace("New Collaboration started: [tid={}, {}]",
+                transactionId,
+                collaboration
+        );
+        return collaboration;
     }
 
     public void inviteUserToCollaboration(@NotNull UUID transactionId, @NotNull UUID collaborationId,
