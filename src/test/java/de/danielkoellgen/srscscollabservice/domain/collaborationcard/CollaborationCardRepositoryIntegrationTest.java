3c948ed4d948bcffded0966a19124a189458568b
dadepu <danielkoellgen@gmail.com>
2022-04-22 10:44:11 +0200

    test: remove outdated tests

--- src/test/java/de/danielkoellgen/srscscollabservice/domain/collaborationcard/CollaborationCardRepositoryIntegrationTest.java
+++
@@ -1,8 +0,0 @@
-package de.danielkoellgen.srscscollabservice.domain.collaborationcard;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class CollaborationCardRepositoryIntegrationTest {
-
-}
